5b838c768e64cab3e6046233473a9d8a072ae4a8
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-10-03 10:53:02 +0800

    revised

--- BasicLearning/Goto.java
+++ BasicLearning/Goto.java
@@ -1,19 +1,21 @@
 package BasicLearning;
+
 public class Goto {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // break
-        int [] intArr = {1, 2, 3, 4, 5};
-        for(int i : intArr ){
-            if(i == 3){
+        int[] intArr = {1, 2, 3, 4, 5};
+        for (int i : intArr) {
+            if (i == 3) {
                 break;
             }
 //            System.out.println(i);
         }
 
         // 標籤式 break
-        loop : for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
-                if(i==2){
+        loop:
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i == 2) {
                     break loop;
                 }
 //                System.out.println(i+j);
@@ -22,9 +24,9 @@ public class Goto {
 
         // continue
         //當程式執行到 continue，則會被強迫直接跳到下一次循環，原本 continue 若下方還有程式則會全部略過。
-        int [] intArr2 = {1, 2, 3, 4, 5};
-        for(int i : intArr2 ){
-            if(i == 3){
+        int[] intArr2 = {1, 2, 3, 4, 5};
+        for (int i : intArr2) {
+            if (i == 3) {
                 continue;
             }
 //            System.out.println(i);
@@ -34,7 +36,7 @@ public class Goto {
         outerLoop:
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                if (i==1 || i==3) {
+                if (i == 1 || i == 3) {
                     continue outerLoop;
                 }
                 System.out.println(i);
